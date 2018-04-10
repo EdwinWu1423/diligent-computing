12f88254f55aa0e1f3dc0c696bd17c344e241ac5
tina <tinaw425@gmail.com>
2018-04-09 21:28:02 -0400

    fixed copy constructor

--- Maze.java
+++ Maze.java
@@ -92,12 +92,11 @@ public class Maze {
 
         // throw new java.lang.RuntimeException(
         //     "Write code to copy the maze[][] array and rankCount.");
-        
+        rankCount = old.rankCount;       
         maze = new int[MAX_RANKS][];
         for(int i = 0; i < rankCount; i++) {
             maze[i] = old.maze[i].clone();
         }
-        rankCount = old.rankCount;
     }
 
 
