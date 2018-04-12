1d4a6c1c2757f18c9e2a9aa057e6345a3af35503
tina <tinaw425@gmail.com>
2018-04-11 21:41:11 -0400

    updated UserOfMazeSolver header comments

--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -1,7 +1,13 @@
 /**
   Test MazeSolver class.
+  
+  Requires command line arguments:
+  o  the name of a file containing a maze.
+  o  the rank and file where an explorer is starting
 
-   */
+  For example,
+      java UserOfMaze mazes/4cell_treasureWest.txt -1 -1  # no explorer
+*/
 
 public class UserOfMazeSolver {
     private static Displayer displayer;
